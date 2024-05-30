3e8483a8d6a05176cf39e356948a14444601c35b
Patrick Würsten <patrick@wuersten.name>
2024-05-30 16:01:13 +0200

    impressum

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,5 +1,6 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import logo from "../Jasslogo.png";
+import fhnw from "../logofhnw.png";
 import "../Styles.css";
 
 export const HeaderComponent = (props) => {
@@ -8,7 +9,8 @@ export const HeaderComponent = (props) => {
     <AppBar position="static">
       <Toolbar className="App-header">
         <img style={{ height: 60 }} src={logo} alt="logo" />
-        <div className="HeaderText" style={{ marginLeft: '250px' }}>Das Schweizer Jassgame</div>
+        <img style={{ height: 60, marginLeft: '40px'}} src={fhnw} alt="fhnw" />
+        <div className="HeaderText" style={{ marginLeft: '200px' }}>Das Schweizer Jassgame</div>
         <div className="HeaderText" id="Username">
           <Typography variant="subtitle1">{props.username}</Typography>
         </div>
