6527e6aacec3295f6371b323af3824342628964f
Dhalu-the-great <doesel1000@gmx.ch>
2024-05-30 10:06:34 +0200

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,3 @@
-import { FooterComponent } from "./FooterComponent";
 import JassGame, { TableComponent } from "./TableComponent";
 import { HeaderComponent } from "../HeaderComponent";
 
@@ -7,7 +6,6 @@ export const Main = (props) => {
     <div className="App">
       <HeaderComponent username={props.username} />
       <JassGame />
-      <FooterComponent />
     </div>
   );
 };
