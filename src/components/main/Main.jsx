2625e9e51d1fa9ef51349b24d6ac34f5688d5a87
Dhalu-the-great <doesel1000@gmx.ch>
2024-05-30 09:58:03 +0200

    Game hands dealt

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,12 @@
 import { FooterComponent } from "./FooterComponent";
-import { TableComponent } from "./TableComponent";
+import JassGame, { TableComponent } from "./TableComponent";
 import { HeaderComponent } from "../HeaderComponent";
 
 export const Main = (props) => {
   return (
     <div className="App">
       <HeaderComponent username={props.username} />
-      <TableComponent tableData={props.tableData} />
+      <JassGame />
       <FooterComponent />
     </div>
   );
