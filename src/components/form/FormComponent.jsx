8001d096eef9e38b60422d1110fd567813d0271c
Sina Kägi <sina.kaegi@students.fhnw.ch>
2024-05-30 15:00:51 +0200

    karte zoom

--- src/components/form/FormComponent.jsx
+++ src/components/form/FormComponent.jsx
@@ -8,32 +8,27 @@ import cantonsGeoJSON from "./../../data/kantone.json";
 
 
 export const FormComponent = (props) => {
- /* useEffect(() => {
-    console.log("fetching...");
-    const pfad = "./data/kantone.geojson";
-    fetch(pfad)
-     .then(res => res.json())
-     .then(json => {
-      // console.log(json);
-      // const cantonsGeoJSON = json;
-      // console.log(cantonsGeoJSON)
-      setData(json)
-     })
-  }, [])
-
-  console.log(cantonsGeoJSON);*/
-
   return (
     <>
       <HeaderComponent username={props.username} />
-    <MapContainer center={[46, 7]} zoom={3} scrollWheelZoom={true} style={{ width: "100%", height: "500px" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.esri.com/en-us/home">Esri</a>, USGS, NOAA'
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}"
-      />
-     <GeoJSON data={cantonsGeoJSON} />
-    </MapContainer>
+      <div style={{ height: "calc(100vh - 135px)" }}>
+        <MapContainer
+          center={[46.8182, 8.2275]}
+          zoom={8}
+          scrollWheelZoom={false}
+          style={{ width: "100%", height: "100%" }}
+          minZoom={7}
+          maxZoom={10}
+          maxBounds={[[45.8182, 5.2275], [47.8182, 11.2275]]}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.esri.com/en-us/home">Esri</a>, USGS, NOAA'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}"
+          />
 
+          <GeoJSON data={cantonsGeoJSON} />
+        </MapContainer>
+      </div>
       <FooterComponent />
     </>
   );
