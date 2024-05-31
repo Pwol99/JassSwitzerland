24ab622fa0e4e033601f4fe0707923070144b05a
Patrick Würsten <patrick@wuersten.name>
2024-05-31 08:09:38 +0200

    diverse form anpassungen

--- src/components/form/FormComponent.jsx
+++ src/components/form/FormComponent.jsx
@@ -1,41 +1,65 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import React, { useState } from 'react';
 import "../../Styles.css";
 import { HeaderComponent } from "../HeaderComponent";
 import { FooterComponent } from "../FooterComponent";
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import cantonsGeoJSON from "./../../data/kantone.json";
 import { useNavigate } from "react-router-dom";
 
+// Funktion zum dynamischen Importieren von Bildern aus dem Dateisystem
+const importImages = (name) => {
+  try {
+    return require(`../../data/Flag_of_Canton_of_${name}.png`);
+  } catch (error) {
+    // Wenn das Bild nicht gefunden wurde, gib ein Standardbild zurück
+    return require(`../../data/default.png`);
+  }
+};
+
 export const FormComponent = (props) => {
   const navigate = useNavigate();
+  const [popupData, setPopupData] = useState(null);
 
   const handleMainPageNavigation = () => {
     console.log("Button clicked");
     navigate("/main");
   };
 
-  const PopupContent = ({ feature }) => {
-    const kanType = feature.properties.Jasskarten_typ;
-    const language = kanType === 'ger' ? 'Deutsch' : 'Französisch';
+  const handleFeatureClick = (feature) => {
+    setPopupData(feature.properties);
+  };
 
+  const PopupContent = () => {
+    if (!popupData) return null;
+    const kanType = popupData.Jasskarten_typ;
+    const language = kanType === 'ger' ? 'Deutsch' : 'Französisch';
+  
     return (
-      <div>
-        <b>Kanton:</b> {feature.properties.NAME}<br/>
-        <b>Jasskarten Typ:</b> {language}<br/>
-        <button onClick={handleMainPageNavigation}>Zur Hauptseite</button>
+      <div className="popup-content">
+        <div className="popup-text">
+          <b>Kanton:</b> {popupData.NAME}<br/>
+          <b>Jasskarten Typ:</b> {language}<br/>
+          <button onClick={handleMainPageNavigation}>Zur Hauptseite</button>
+        </div>
+        <div className="popup-flag">
+          {/* Lade das Bild dynamisch */}
+          <img src={importImages(popupData.NAME)} width="100" alt={popupData.NAME} />
+        </div>
       </div>
     );
   };
+  
 
   const geoJsonStyle = {
     color: '#000000',
     weight: 1,
-    fill: true, // Fill the area
-    fillOpacity: 0.5, // Set fill opacity to make the area clickable
+    fill: true,
+    fillOpacity: 0.5,
     fillColor: '#FFFFFF',
-    interactive: true // Enable mouse events on the entire polygon area
+    interactive: true
   };
 
   return (
@@ -45,17 +69,17 @@ export const FormComponent = (props) => {
         <MapContainer
           center={[46.8182, 8.2275]}
           zoom={8}
-          scrollWheelZoom={false} // Disable scroll wheel zoom
-          dragging={false} // Disable dragging
-          touchZoom={false} // Disable touch zoom
-          doubleClickZoom={false} // Disable double click zoom
-          tap={false} // Disable tap
+          scrollWheelZoom={false}
+          dragging={false}
+          touchZoom={false}
+          doubleClickZoom={false}
+          tap={false}
           style={{ width: "100%", height: "100%" }}
           minZoom={8}
           maxZoom={8}
           maxBounds={[[45.8182, 5.2275], [47.8182, 11.2275]]}
           zoomControl={false}
-          preferCanvas={true} // Prefer canvas rendering
+          preferCanvas={true}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.esri.com/en-us/home">Esri</a>, USGS, NOAA'
@@ -65,13 +89,14 @@ export const FormComponent = (props) => {
             data={cantonsGeoJSON}
             style={geoJsonStyle}
             onEachFeature={(feature, layer) => {
-              if (feature.properties && feature.properties.NAME) {
-                const popupContent = <PopupContent feature={feature} />;
-                layer.bindPopup(ReactDOMServer.renderToString(popupContent)); // Bind popup to each feature
-              }
+              layer.on('click', () => handleFeatureClick(feature));
             }}
           />
         </MapContainer>
+        <Popup open={!!popupData} closeOnDocumentClick onClose={() => setPopupData(null)} style={{ width: "100px", height: "150px" }}>
+  <PopupContent />
+</Popup>
+
       </div>
       <FooterComponent />
     </>
