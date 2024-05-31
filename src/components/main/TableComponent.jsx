a99e74c41500032ed59be27e0bad9c7e3e875a3e
Dhalu-the-great <doesel1000@gmx.ch>
2024-05-31 14:14:59 +0200

    server ohne points ;(

--- src/components/main/TableComponent.jsx
+++ src/components/main/TableComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import teppich from "./jass_teppich.png"; // Import the image
 import io from 'socket.io-client';
+import { Modal, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material'; // Import Material-UI components
 
 // Connect to the backend WebSocket server
 const socket = io('http://localhost:3001');
@@ -12,6 +13,10 @@ const joinGame = (playerName) => {
 // Define the cards and their values
 const suits = ['Diamond', 'Heart', 'Spade', 'Klubs'];
 const values = ['6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+const trumpSuitLabels = {
+  fra: ['Karo','Herz', 'Pik','Kreuz'],
+  ger: ['Eichel', 'Rose', 'Schilte', 'Schelle']
+};
 
 // Map each card to an index for the image
 const getCardIndex = (card) => {
@@ -36,6 +41,8 @@ const JassGame = (props) => {
   const [trumpSuit, setTrumpSuit] = useState(null);
   const [currentTurn, setCurrentTurn] = useState(0);
   const [playerIndex, setPlayerIndex] = useState(null);
+  const [trumpSuitOptions, setTrumpSuitOptions] = useState(['Diamond', 'Heart', 'Spade', 'Klubs']);
+  const [openTrumpModal, setOpenTrumpModal] = useState(false);
 
   useEffect(() => {
     console.log("run joinGame");
@@ -52,6 +59,7 @@ const JassGame = (props) => {
       setCurrentTurn(updatedGameState.currentTurn || 0);
       // Update other relevant state properties as needed
     });
+    
 
     socket.on('playerIndex', (index) => {
       setPlayerIndex(index);
@@ -64,12 +72,6 @@ const JassGame = (props) => {
       socket.off('playerIndex');
     };
   }, []);
-
-  const handleTrumpSuitSelect = (e) => {
-    if (hands[playerIndex] && hands[playerIndex].length === 9) {
-      setTrumpSuit(e.target.value);
-    }
-  };
 
 // Function to play a card
 const playCard = (index) => {
@@ -94,78 +96,115 @@ useEffect(() => {
     setPlayedCards(cards);
   });
 
+  
   // Clean up the event listener when the component unmounts
   return () => {
     socket.off('playedCards');
   };
 }, []);
 
+ useEffect(() => {
+  // Listen for the 'chooseTrumpSuit' event from the server
+  socket.on('chooseTrumpSuit', () => {
+    // Open the modal for choosing trump suit
+    setOpenTrumpModal(true);
+  });
 
-  return (
-    <div style={{ backgroundColor: 'white', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ marginTop: '20px', color: 'black' }}>
-        <label htmlFor="trumpSuit" style={{ marginLeft: '10px' }}>Select Trump Suit:</label>
-        <select id="trumpSuit" onChange={handleTrumpSuitSelect} style={{ marginLeft: '10px' }}>
-          <option value="">None</option>
-          {suits.map((suit, index) => (
-            <option key={index} value={suit}>{suit}</option>
-          ))}
-        </select>
-      </div>
-      <div style={{
-        marginTop: '20px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        flex: '1',
-        width: '100%',
-        height: '100%',
-        background: `url(${teppich}) center center no-repeat`, // Use the imported image
-        backgroundSize: 'cover'
-      }}>
-        {playedCards?.filter(card => card !== undefined)?.map((card, index) => (
-          <img
-            key={index}
-            src={getCardImage(getCardIndex(card), props.jasskarten_typ)}
-            alt={`${card.value} of ${card.suit}`}
-            style={{
-              width: '80px',
-              position: 'absolute',
-              top: 'calc(50% - 40px)',
-              left: 'calc(50% - 40px)',
-              transform: `rotate(${index === 0 ? '0deg' : index === 1 ? '90deg' : index === 2 ? '0deg' : '90deg'})
-                          translate(${card.index === 0 ? '0px, 50px' : card.index === 1 ? '0px, -50px' : card.index === 2 ? '0px, -50px' : '0px, 50px'})`, // Adjusted rotation and translation
-              zIndex: index === 0 ? '0' : index === 1 ? '1' : index === 2 ? '2' : '3',
-              cursor: 'pointer' // Cursor style for indicating clickability
-            }}
-          />
-        ))}
-      </div>
-      <div style={{ marginTop: '20px', marginBottom: '20px', color: 'black' }}>
-        <div>player index: {playerIndex}</div>
-        <h2>{props.playername}</h2>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          {hands[playerIndex] && hands[playerIndex].length > 0 ? (
-            hands[playerIndex].map((card, index) => {
-              const cardIndex = getCardIndex(card);
-              return (
-                <div key={index} style={{ margin: '5px', cursor: 'pointer' }} onClick={() => playCard(index)}>
-                  <img
-                    src={getCardImage(cardIndex, props.jasskarten_typ)}
-                    alt={`${card.value} of ${card.suit}`}
-                    style={{ width: '80px' }}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <p>No cards to display</p>
-          )}
-        </div>
+  // Clean up the event listener when the component unmounts
+  return () => {
+    socket.off('chooseTrumpSuit');
+  };
+}, []);
+
+const handleCloseTrumpModal = () => {
+  // Close the modal for choosing trump suit
+  setOpenTrumpModal(false);
+};
+
+const handleTrumpSuitSelect = (e) => {
+  const chosenSuit = e.target.value;
+  if (chosenSuit) {
+    // Emit the chosen trump suit to the server
+    socket.emit('selectTrumpSuit', chosenSuit);
+    setOpenTrumpModal(false); // Close the modal after selection
+  }
+};
+
+
+
+return (
+  <div style={{ backgroundColor: 'white', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{
+      marginTop: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'relative',
+      flex: '1',
+      width: '100%',
+      height: '100%',
+      background: `url(${teppich}) center center no-repeat`, // Use the imported image
+      backgroundSize: 'cover'
+    }}>
+      {playedCards?.filter(card => card !== undefined)?.map((playedCard, index) => (
+        <img
+          key={index}
+          src={playedCard && playedCard.card ? getCardImage(getCardIndex(playedCard.card), props.jasskarten_typ) : ''}
+          alt={playedCard && playedCard.card ? `${playedCard.card.value} of ${playedCard.card.suit}` : ''}
+          style={{
+            width: '80px',
+            position: 'absolute',
+            top: 'calc(50% - 40px)',
+            left: 'calc(50% - 40px)',
+            transform: `rotate(${index === 0 ? '0deg' : index === 1 ? '90deg' : index === 2 ? '0deg' : '90deg'})
+                        translate(${index === 0 ? '0px, 50px' : index === 1 ? '0px, -50px' : index === 2 ? '0px, -50px' : '0px, 50px'})`,
+            zIndex: index === 0 ? '0' : index === 1 ? '1' : index === 2 ? '2' : '3',
+            cursor: 'pointer'
+          }}
+        />
+      ))}
+    </div>
+
+    <div style={{ marginTop: '20px', marginBottom: '20px', color: 'black' }}>
+      <div>player index: {playerIndex}</div>
+      <h2>{props.playername}</h2>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {hands[playerIndex] && hands[playerIndex].length > 0 ? (
+          hands[playerIndex].map((card, index) => {
+            const cardIndex = getCardIndex(card);
+            return (
+              <div key={index} style={{ margin: '5px', cursor: 'pointer' }} onClick={() => playCard(index)}>
+                <img
+                  src={getCardImage(cardIndex, props.jasskarten_typ)}
+                  alt={`${card.value} of ${card.suit}`}
+                  style={{ width: '80px' }}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <p>No cards to display</p>
+        )}
       </div>
     </div>
-  );
+
+    {/* Trump Suit Selection Modal */}
+    {openTrumpModal && (
+  <div className="modal" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div className="modal-content" style={{ backgroundColor: '#fff', padding: '20px', borderRadius: '10px' }}>
+      <h2>Choose Trump Suit</h2>
+      <select onChange={handleTrumpSuitSelect} defaultValue="" style={{ width: '100%', padding: '10px', fontSize: '16px', marginBottom: '20px' }}>
+        <option value="" disabled hidden>Select Trump Suit</option>
+        {trumpSuitLabels[props.jasskarten_typ].map((suit, index) => (
+          <option key={index} value={suit}>{suit}</option>
+        ))}
+      </select>
+      <button onClick={handleCloseTrumpModal}>Cancel</button>
+    </div>
+  </div>
+)}
+  </div>
+);
 };
 
 export default JassGame;
